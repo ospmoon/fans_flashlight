5a01dcba267ba522a32626412d8c79c23e4fd631
Alexandr Ostapyuk <ao@99sensors.com>
2025-10-02 15:03:05 +0300

    v2 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "osp.moon.funsflashlight"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     buildTypes {
